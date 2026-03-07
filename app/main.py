58e824a466a38d122eb8cd2d6437f3ce05d93c50
Álvaro José <josealvaro3421@gmail.com>
2026-03-07 20:18:45 -0300

    feat: api post method for product table

--- app/main.py
+++ app/main.py
@@ -1,6 +1,12 @@
 from fastapi import FastAPI
+from app.db.base import Base
+from app.db.session import engine
+from app.routers.product_router import router as product_router
+
+Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+app.include_router(product_router)
 
 @app.get("/")
 def read_root():
