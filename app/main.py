2da84f960880a30091d0efc721419eb49d3ab6cf
Álvaro José <josealvaro3421@gmail.com>
2026-03-08 17:17:00 -0300

    add: implemented user router in main.py

--- app/main.py
+++ app/main.py
@@ -2,11 +2,13 @@ from fastapi import FastAPI
 from app.db.base import Base
 from app.db.session import engine
 from app.routers.product_router import router as product_router
+from app.routers.user_router import router as user_router
 
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 app.include_router(product_router)
+app.include_router(user_router)
 
 @app.get("/")
 def read_root():
