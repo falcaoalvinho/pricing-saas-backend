29e320c9e6e20db3f959275bc4b1a04c4b77a551
Álvaro José <josealvaro3421@gmail.com>
2026-03-08 15:10:16 -0300

    add: user model content

---
+++ app/models/user.py
@@ -0,0 +1,13 @@
+from sqlalchemy import Column, Integer, String, Float
+from app.db.base import Base
+from datetime import datetime
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, nullable=False)
+    email = Column(String, nullable=False, unique=True)
+    hashed_password = Column(String, nullable=False) 
+    is_active = Column(Bolean, nullable=False)
+    created_at = Column(DateTime, default=datetime.now)
