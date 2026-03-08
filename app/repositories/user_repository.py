be55d34faec3258bbca1a56a06a636bd9366684c
Álvaro José <josealvaro3421@gmail.com>
2026-03-08 17:13:54 -0300

    add: user repository

---
+++ app/repositories/user_repository.py
@@ -0,0 +1,16 @@
+from sqlalchemy.orm import Session
+from app.models.user import User
+from app.core.security import create_hash
+
+def create_user(db: Session, user_data):
+    user = User(
+        name=user_data.name,
+        email=user_data.email,
+        hashed_password=create_hash(user_data.password)
+    )
+
+    db.add(user)
+    db.commit()
+    db.refresh(user)
+
+    return user
