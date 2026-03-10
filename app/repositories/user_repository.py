a1a34e76695a20645409e317877e083c1b055a72
Álvaro José <josealvaro3421@gmail.com>
2026-03-10 15:55:46 -0300

    add: update function in repository layer, finished inplementation of update workflow on user data

--- app/repositories/user_repository.py
+++ app/repositories/user_repository.py
@@ -20,5 +20,19 @@ def read_user_list(db: Session):
     response = db.query(User).all()
     return {"users": response}
 
-def read_user(db, user_id):
+def read_user(db: Session, user_id: int):
     return db.query(User).filter(User.id == user_id).first()
+
+def update_user(db: Session, user_id: int, new_data: dict):
+    data = new_data.model_dump(exclude_unset=True)
+
+    user = db.query(User).filter(User.id == user_id).first()
+
+    for key, value in data.items():
+        setattr(user, key, value)
+
+    db.add(user)
+    db.commit()
+    db.refresh(user)
+
+    return user
