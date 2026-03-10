b205b3b78c7f8b53057b9865fc0382a5ffe97b24
Álvaro José <josealvaro3421@gmail.com>
2026-03-10 15:53:58 -0300

    add: new route for put requests on users

--- app/routers/user_router.py
+++ app/routers/user_router.py
@@ -1,8 +1,9 @@
 from fastapi import APIRouter, Depends
-from sqlalchemy.orm import Session
-from app.schemas.user import UserCreate, UserResponse, UserListResponse
-from app.services import user_service
 from app.db.session import SessionLocal
+from sqlalchemy.orm import Session
+
+from app.schemas.user import UserCreate, UserResponse, UserListResponse, UserUpdate
+from app.services import user_service
 
 router = APIRouter(prefix="/users")
 
@@ -24,3 +25,7 @@ def get_user_list(db: Session = Depends(get_db)):
 @router.get("/{user_id}", response_model=UserResponse)
 async def get_user(user_id: int, db: Session = Depends(get_db)):
     return user_service.read_user(db, user_id)
+
+@router.put("/{user_id}", response_model=UserResponse)
+def update_user(user_id: int, new_data: UserUpdate, db: Session = Depends(get_db)):
+    return user_service.update_user(db, user_id, new_data)
