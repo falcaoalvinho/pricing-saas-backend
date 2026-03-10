33c415cf2f5bbdcf70faef1b7a8859418af1a8f5
Álvaro José <josealvaro3421@gmail.com>
2026-03-09 23:15:40 -0300

    add: new schemas for user, UserResponse and UserListResponse

--- app/schemas/user.py
+++ app/schemas/user.py
@@ -1,6 +1,18 @@
 from pydantic import BaseModel
+from datetime import datetime
+from typing import List
 
 class UserCreate(BaseModel):
     name: str
     email: str
     password: str
+
+class UserResponse(BaseModel):
+    id: int
+    name: str
+    email: str
+    is_active: bool
+    created_at: datetime
+
+class UserListResponse(BaseModel):
+    users: List[UserResponse]
