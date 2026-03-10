9d6eccd5e8b977ec041a93e7a7e72f3f1a223aa3
Álvaro José <josealvaro3421@gmail.com>
2026-03-10 15:54:47 -0300

    add: update methodo for user data in service layer

--- app/services/user_service.py
+++ app/services/user_service.py
@@ -10,7 +10,7 @@ def read_user_list(db):
     return user_repository.read_user_list(db)
 
 def read_user(db, user_id):
-    return user_repository.read_user(
-        db,
-        user_id
-        )
+    return user_repository.read_user(db, user_id)
+
+def update_user(db, user_id, new_data):
+    return user_repository.update_user(db, user_id, new_data)
