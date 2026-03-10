0f2ea9f837b01a1b863f92bab191fdffeef524ef
Álvaro José <josealvaro3421@gmail.com>
2026-03-09 23:17:02 -0300

    add: routes for get all requisitions and fix in post method bad response for user entities

--- app/routers/user_router.py
+++ app/routers/user_router.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
-from app.schemas.user import UserCreate
+from app.schemas.user import UserCreate, UserResponse, UserListResponse
 from app.services import user_service
 from app.db.session import SessionLocal
 
@@ -13,6 +13,10 @@ def get_db():
     finally:
         db.close()
 
-@router.post("/users")
+@router.get("/", response_model=UserListResponse)
+def get_users(db: Session = Depends(get_db)):
+    return user_service.read_users(db) 
+
+@router.post("/", response_model=UserResponse)
 def create_product(user: UserCreate, db: Session = Depends(get_db)):
     return user_service.create_user(db, user)
