f27c1b2c5a7b185c1d81fe284ba9b78ad3d37377
Álvaro José <josealvaro3421@gmail.com>
2026-03-12 14:26:57 -0300

    feat: finished CRUD for product entites on all layers

--- app/repositories/product_repository.py
+++ app/repositories/product_repository.py
@@ -15,15 +15,33 @@ def create_product(db: Session, product_data, suggested_price):
 
     return product
 
-
 def read_product_list(db):
-    ...
+    response = db.query(Product).all()
+    return {"products": response}
 
 def read_product(db, product_id):
-    ...
+    return db.query(Product).filter(Product.id == product_id).first()
 
 def update_product(db, product_id, new_data, new_suggested_price):
-    ...
+    data = new_data.model_dump(exclude_unset=True)
+    data["sugested_price"] == new_suggested_price
 
+    product = db.query(product).filter(product.id == product_id).first()
+
+    for key, value in data.items():
+        setattr(product, key, value)
+
+    db.add(product)
+    db.commit()
+    db.refresh(product)
+
+    return product
+
+# TODO: add error handling
 def delete_product(db, product_id):
-    ...
+    product = db.query(Product).filter(Product.id == product_id).first()
+
+    db.delete(product)
+    db.commit()
+
+    return read_product_list(db)
