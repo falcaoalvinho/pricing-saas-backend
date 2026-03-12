5dc0a060cf9a41a2b67094643f5a97dbff748710
Álvaro José <josealvaro3421@gmail.com>
2026-03-12 14:14:17 -0300

    feat: added CRUD functions content for product entities on service layer

--- app/services/product_service.py
+++ app/services/product_service.py
@@ -17,12 +17,17 @@ def create_product(db, product_data):
     )
 
 def read_product_list(db):
-    ...
+    return product_repository.read_product_list(db)
 
 def read_product(db, product_id):
-    ...
+    return product_repository.read_product(db, product_id)
 
 def update_product(db, new_data, product_id):
-    ...
+        new_suggested_price = calculate_price(
+        product_data.cost,
+        product_data.margin_percentage
+    )
+    return product_repository.update_product(db, new_data, product_id, new_suggested_price)
 
-def delete_product(db, product_id)
+def delete_product(db, product_id):
+    return product_repository.delete_product(db, new_data)
