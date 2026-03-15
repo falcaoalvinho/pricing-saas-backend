4a9b5041b6742f2a7695a5efc0c399bbd75b83a5
Álvaro José <josealvaro3421@gmail.com>
2026-03-15 17:07:56 -0300

    feat: password verification function using CryptContext

--- app/core/security.py
+++ app/core/security.py
@@ -2,5 +2,8 @@ from passlib.context import CryptContext
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
-def create_hash(text: str):
+def create_hash(text: str) -> str:
     return pwd_context.hash(text)
+
+def verify_password(plain_password: str, hashed_passaword: str) -> bool:
+    return pwd_context.verify(plain_password, hashed_passaword)
