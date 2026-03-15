41e4b7e341632282a6c318b2d7369b8abdf0cc78
Álvaro José <josealvaro3421@gmail.com>
2026-03-15 20:38:05 -0300

    feat: implementation of autentication logic with JWT and multi-tenant pattern

--- app/main.py
+++ app/main.py
@@ -1,8 +1,10 @@
 from fastapi import FastAPI
+
 from app.db.base import Base
 from app.db.session import engine
 from app.routers.product_router import router as product_router
 from app.routers.user_router import router as user_router
+from app.routers.auth_router import router as auth_router
 
 Base.metadata.create_all(bind=engine)
 
@@ -12,9 +14,10 @@ app = FastAPI(
     version="1.0.0"
 )
 
-app.include_router(product_router)
+app.include_router(auth_router)
 app.include_router(user_router)
+app.include_router(product_router)
 
-@app.get("/", summary="Default", description="Serve para saber se a API está rodando")
+@app.get("/", summary="Default", tags=["Default"], description="Serve para saber se a API está rodando")
 def read_root():
     return {"message": "API running"}
