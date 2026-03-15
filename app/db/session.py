6b1eb9b4bb8105cf1e1fbb0a2b0acbf96d5fe03d
Álvaro José <josealvaro3421@gmail.com>
2026-03-15 17:32:54 -0300

    refactor: removed os lib import, to use core settings

--- app/db/session.py
+++ app/db/session.py
@@ -1,10 +1,9 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-import os
 
-DATABASE_URL = os.getenv("DATABASE_URL")
+from app.core.config import settings
 
-engine = create_engine(DATABASE_URL)
+engine = create_engine(settings.DATABASE_URL)
 
 SessionLocal = sessionmaker(
     autocommit=False,
