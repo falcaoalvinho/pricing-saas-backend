0aeef7f5f3bdaa3b3a7e59c11a05d6457e41ebdc
Álvaro José <josealvaro3421@gmail.com>
2026-03-22 22:15:06 -0300

    feat: update on the product model

--- app/models/product.py
+++ app/models/product.py
@@ -5,11 +5,11 @@ from app.db.base import Base
 class Product(Base):
     __tablename__ = "products"
 
-    id = Column(Integer, primary_key=True, index=True)
-    user_id = Column(Integer, ForeignKey("users.id"), nullable=False)
-    name = Column(String, nullable=False)
+    id = Column(Integer, primary_key=True)
+    organization_id = Column(Integer, ForeignKey("organizations.id"))
+    name = Column(String(100), nullable=False)
     cost = Column(Float, nullable=False)
     margin_percentage = Column(Float, nullable=False)
     suggested_price = Column(Float, nullable=False)
 
-    owner = relationship("User", back_populates="products")
+    owner = relationship("Organization", back_populates="products")
