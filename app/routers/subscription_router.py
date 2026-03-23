0200117998968208b38951eb979e7f7db66f67df
Álvaro José <josealvaro3421@gmail.com>
2026-03-22 23:48:43 -0300

    feat: first sketch of subscription router

---
+++ app/routers/subscription_router.py
@@ -0,0 +1,37 @@
+from fastapi import APIRouter, Depends
+from sqlalchemy.orm import Session
+
+from app.schemas.subscription import SubscriptionCreate, SubscriptionResponse, SubscriptionUpdate
+from app.db.session import get_db, 
+from app.services.subscription import subscription_service
+
+router = APIRouter(prefix="/subscription", tags=["Subscriptions"])
+
+db = get_db()
+
+@router_get(
+    "/",
+    summary="Read subscription",
+    description="Retorna a subscription vinculada a organization atual.",
+    reponse_model=SubscriptionResponse
+)
+def read_subscription():
+    return {"mensage": "[subscription] get subscription request was awnswered successfully!"}
+
+@router_update(
+    "/",
+    summary="Update subscription",
+    description="Atualiza a subscription da organization atual.",
+    reponse_model=SubscriptionResponse
+)
+def update_subscription():
+    return {"mensage": "[subscription] update subscription request was awnswered successfully!"}
+
+@router_delete(
+    "/",
+    summary="Delete subscription",
+    description="Delete a subscription da organization.",
+    reponse_model=SubscriptionResponse
+)
+def delete_subscription():
+    return {"mensage": "[subscription] delete subscription request was awnswered successfully!"}
