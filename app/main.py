087ad399109325fb23d70ff5460dd8c9f7849eda
Álvaro José <josealvaro3421@gmail.com>
2026-03-24 19:50:51 -0300

    feat: added new routers on app

--- app/main.py
+++ app/main.py
@@ -5,6 +5,9 @@ from app.db.session import engine
 from app.routers.product_router import router as product_router
 from app.routers.user_router import router as user_router
 from app.routers.auth_router import router as auth_router
+from app.routers.subscription_router import router as subscription_router
+from app.routers.organization_router import router as organization_router
+from app.routers.membership_router import router as membership_router
 
 Base.metadata.create_all(bind=engine)
 
@@ -17,6 +20,9 @@ app = FastAPI(
 app.include_router(auth_router)
 app.include_router(user_router)
 app.include_router(product_router)
+app.include_router(subscription_router)
+app.include_router(organization_router)
+app.include_router(membership_router)
 
 @app.get("/", summary="Default", tags=["Default"], description="Serve para saber se a API está rodando")
 def read_root():
