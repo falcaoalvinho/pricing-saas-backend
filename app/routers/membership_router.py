bb2350602157cc4770a2b3c746555e7c3e0c379c
Álvaro José <josealvaro3421@gmail.com>
2026-03-28 19:13:23 -0300

    fix: on app routers

--- app/routers/membership_router.py
+++ app/routers/membership_router.py
@@ -2,54 +2,53 @@ from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
 
 from app.schemas.membership import MembershipCreate, MembershipResponse, MembershipUpdate
-from app.db.session import get_db, 
-from app.services.membership import membership_service
+from app.db.session import get_db
 
-router = APIRouter(prefix="/memberships", tags=["Organizations"])
+router = APIRouter(prefix="/memberships", tags=["Memberships"])
 
 db = get_db()
 
-@router_post(
+@router.post(
     "/",
     summary="Create membership",
     description="Cria uma nova membership no banco de dados vinculada ao usuário atual e uma organização da qual ele faz parte.",
-    reponse_model=list[MembershipResponse]
+    # response_model=list[MembershipResponse]
 )
 def create_membership():
     return {"mensage": "[membership] create request was answered successfully!"}
 
-@router_get(
+@router.get(
     "/",
     summary="Read all memberships",
     description="Retorna a lista de memberships do usuário atual.",
-    reponse_model=list[MembershipResponse]
+    # response_model=list[MembershipResponse]
 )
 def get_membership_list():
     return {"mensage": "[membership] get all request was answered successfully!"}
 
-@router_get(
+@router.get(
     "/{membership_id}",
     summary="Read membership",
     description="Retorna uma membership se ele pertencer ao usuário atual.",
-    reponse_model=MembershipResponse
+    # response_model=MembershipResponse
 )
 def get_membership():
     return {"mensage": "[membership] get by id request was answered successfully!"}
 
-@router_update(
+@router.put(
     "/{membership_id}",
     summary="Update membership",
     description="Atualiza o registro de uma membership do usuário atual.",
-    reponse_model=MembershipResponse
+    # response_model=MembershipResponse
 )
 def update_membership():
     return {"mensage": "[membership] update request was answered successfully!"}
 
-@router_delete(
+@router.delete(
     "/{membership_id}",
     summary="Delete Membership",
     description="Deleta o registro de uma membership do usuário atual.",
-    reponse_model=list[MembershipResponse]
+    # response_model=list[MembershipResponse]
 )
 def delete_membership():
     return {"mensage": "[membership] delete request was answered successfully!"}
--- app/routers/subscription_router.py
+++ app/routers/subscription_router.py
@@ -2,36 +2,35 @@ from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
 
 from app.schemas.subscription import SubscriptionCreate, SubscriptionResponse, SubscriptionUpdate
-from app.db.session import get_db, 
-from app.services.subscription import subscription_service
+from app.db.session import get_db
 
 router = APIRouter(prefix="/subscription", tags=["Subscriptions"])
 
 db = get_db()
 
-@router_get(
+@router.get(
     "/",
     summary="Read subscription",
     description="Retorna a subscription vinculada a organization atual.",
-    reponse_model=SubscriptionResponse
+    # response_model=SubscriptionResponse
 )
 def read_subscription():
     return {"mensage": "[subscription] get subscription request was awnswered successfully!"}
 
-@router_update(
+@router.put(
     "/",
     summary="Update subscription",
     description="Atualiza a subscription da organization atual.",
-    reponse_model=SubscriptionResponse
+    # response_model=SubscriptionResponse
 )
 def update_subscription():
     return {"mensage": "[subscription] update subscription request was awnswered successfully!"}
 
-@router_delete(
+@router.delete(
     "/",
     summary="Delete subscription",
     description="Delete a subscription da organization.",
-    reponse_model=SubscriptionResponse
+    # response_model=SubscriptionResponse
 )
 def delete_subscription():
     return {"mensage": "[subscription] delete subscription request was awnswered successfully!"}
