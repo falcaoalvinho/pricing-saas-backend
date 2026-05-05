99e13539d9d4e79d56bad0ef41316cbcbb82ba38
Álvaro José <josealvaro3421@gmail.com>
2026-05-05 20:31:44 -0300

    refactor: on main.py

--- app/main.py
+++ app/main.py
@@ -1,19 +1,17 @@
+# DEPENDENCIES IMPORTS
 from fastapi import FastAPI
 
+# PROJECT GENERAL IMPORTS
 from app.db.base import Base
-from app.models.user import User
-from app.models.organization import Organization
-from app.models.membership import Membership
-from app.models.product import Product
-from app.models.subscription import Subscription
-
 from app.db.session import engine
-from app.routers.product_router import router as product_router
-from app.routers.user_router import router as user_router
+
+# PROJECT ROUTERS IMPORTS
 from app.routers.auth_router import router as auth_router
-from app.routers.subscription_router import router as subscription_router
-from app.routers.organization_router import router as organization_router
 from app.routers.membership_router import router as membership_router
+from app.routers.organization_router import router as organization_router
+from app.routers.product_router import router as product_router
+from app.routers.subscription_router import router as subscription_router
+from app.routers.user_router import router as user_router
 
 Base.metadata.create_all(bind=engine)
 
@@ -29,7 +27,3 @@ app.include_router(organization_router)
 app.include_router(membership_router)
 app.include_router(subscription_router)
 app.include_router(product_router)
-
-# @app.get("/", summary="Default", tags=["Default"], description="Serve para saber se a API está rodando")
-# def read_root():
-#     return {"message": "API running"}
